819d9f38720003a658afaf3723f8da4a80ff073c
reiyachinen <rei.c0523@gmail.com>
2023-10-22 00:00:12 +0900

    made todo

--- work/public/index.php
+++ work/public/index.php
@@ -6,7 +6,7 @@ define('DB_PASS', 'myapppass');
 
 try {
   $pdo =new PDO(
-    DNS,
+    DSN,
     DB_USER,
     DB_PASS,
     [
@@ -26,8 +26,24 @@ try {
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
   <h1>Todos</h1>
+
+  <ul>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+    <li>
+      <input type="checkbox" checked>
+      <span class="done">Title</span>
+    </li>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+  </ul>
 </body>
 </html>
