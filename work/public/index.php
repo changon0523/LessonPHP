4e835f9fd42982b936bd54cc9b209d180d9675c3
reiyachinen <rei.c20523@gmail.com>
2024-01-18 01:38:35 +0900

    CSRF

--- work/public/index.php
+++ work/public/index.php
@@ -1,8 +1,13 @@
 <?php
 
+session_start();
+
 define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -25,6 +30,23 @@ function h($str)
   return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
+function createToken()
+{
+  if (!isset($_SESSION['token'])){
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken()
+{
+  if (
+    empty($_SESSION['token']) ||
+    $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
+  ) {
+    exit('Invalid post request');
+  }
+}
+
 function addTodo($pdo)
 {
   $title = (filter_input(INPUT_POST, "title"));
@@ -45,7 +67,11 @@ function getTodos($pdo)
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  validateToken();
   addTodo($pdo);
+  // 再読み込みするとindex.phpがpostされてしまうので、postではない形式でアクセスする
+  header('Location: ' . SITE_URL);
+  exit;
 }
 
 $todos = getTodos($pdo);
@@ -63,6 +89,7 @@ $todos = getTodos($pdo);
 <!-- todoを追加するためのフォーム -->
   <form action="" method="post">
     <input type="text" name="title" placeholder="Type new todo">
+    <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
   </form>
 
   <ul>
